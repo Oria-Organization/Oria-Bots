81f062524c9cf8f91d21b62ca634182d661f873b
Hunterdivin30 <divinehunter30@gmail.com>
2025-09-23 18:59:24 +0200

    Update main.py
    Ajout des lanceurs de bot

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-import discord
-from discord.ext import commands
+import asyncio
 from oria_communauté.bot1.py import bot1.py
 from nexara_officiel.bot2.py import bot2.py
+
+#Lancement des bots
+async def main():
+    await asyncio.gather(
+        bot1.start("TOKEN_BOT1"),
+        bot2.start("TOKEN_BOT2"))
+asyncio.run(main())

a3b23e96211e34cdeb59a8d7fd42dfb2069d9a10
Rababio <139578511+Rababio4579@users.noreply.github.com>
2025-09-24 16:25:02 +0200

    add intents

--- nexara_officiel/bot2.py
+++ nexara_officiel/bot2.py
@@ -2,7 +2,7 @@ import discord
 
 intents = discord.Intents.default()
 intents.message_content = True
-bot2 = discord.Client()
+bot2 = discord.Client(intents=intents)
 
 async def start(token):
     await bot2.start(token)

2b85ecc312b4908538c7576dbf134d4f5c5b2430
Rababio <139578511+Rababio4579@users.noreply.github.com>
2025-09-24 16:25:29 +0200

    add intents

--- oria_communauté/bot1.py
+++ oria_communauté/bot1.py
@@ -2,7 +2,7 @@ import discord
 
 intents = discord.Intents.default()
 intents.message_content = True
-bot1 = discord.Client()
+bot1 = discord.Client(intents=intents)
 
 async def start(token):
     await bot1.start(token)

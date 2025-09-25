f1b868cd043c056dec91061bb161e627648b07eb
Hunterdivin30 <divinehunter30@gmail.com>
2025-09-25 16:03:12 +0200

    Update bot1.py

--- oria_communauté/bot1.py
+++ oria_communauté/bot1.py
@@ -1,8 +1,14 @@
 import discord
 
-intents = discord.Intents.default()
-intents.message_content = True
-bot1 = commands.Bot(command_prefix="!", intents=intents)
+def get_intents():
+    intents = discord.Intents.default()
+    intents.message_content = True
+    intents.members = True
+    intents.dm_messages = True
+    intents.guilds = True
+    return intents
+
+bot2 = commands.Bot(command_prefix="!", intents=get_intents())
 
 async def start(token):
     await bot1.start(token)
